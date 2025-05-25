9eb696a7ae6e31021ad76b6c5d04d9fb324bfabb
cordob4a <cordoba.julian.alexis@gmail.com>
2025-05-25 18:34:44 -0300

    actualizacion de html y codigo no funcional

--- app.py
+++ app.py
@@ -1,62 +1,86 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import re
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Adproff/Desktop/Prog/python/instance/clinica.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clinica.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Paciente(db.Model):
     __tablename__ = 'paciente'
-    id = db.Column(db.Integer, primary_key=True)
-    nombre_paciente = db.Column(db.String(100), nullable=False)
-    nombre_medico = db.Column(db.String(100), nullable=False)
-    fecha_turno = db.Column(db.String(100))
+    dni = db.Column(db.BigInteger, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    tel = db.Column(db.String(100), nullable=False)
+
     def __repr__(self):
-        return f"<Paciente {self.nombre_paciente}- Médico: {self.nombre_medico}>"
+        return f"<Paciente: {self.nombre} - DNI: {self.dni}>"
+
+class Turno(db.Model):
+    __tablename__ = 'turno'
+    id = db.Column(db.Integer, primary_key=True)
+    dni_paciente = db.Column(db.BigInteger, db.ForeignKey('paciente.dni'), nullable=False)
+    matricula = db.Column(db.BigInteger, nullable=False)
+    fecha = db.Column(db.String(20), nullable=False)
+    hora = db.Column(db.String(10), nullable=False)
+    estado = db.Column(db.String(20), nullable=False)
+
 
 @app.route('/')
-def index():
-    pacientes = Paciente.query.all()
-    #print("index ",pacientes)
-    return render_template('/index.html', pacientes=pacientes)
+def home():
+    return render_template('index.html')
+
+
+@app.route('/cargar_paciente', methods=['GET', 'POST'])
+def cargar_paciente():
+    if request.method == 'POST':
+        dni = int(request.form['dni'])
+        nombre = request.form['nombre']
+        tel = request.form['tel']
+        paciente = Paciente(dni=dni, nombre=nombre, tel=tel)
+        db.session.add(paciente)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('cargar_paciente.html')
+
+
+@app.route('/cargar_turno', methods=['GET', 'POST'])
+def cargar_turno():
+    if request.method == 'POST':
+        dni = int(request.form['dni'])
+        matricula = int(request.form['matricula'])
+        fecha = request.form['fecha']
+        hora = request.form['hora']
+        estado = request.form['estado']
+        turno = Turno(dni_paciente=dni, matricula=matricula, fecha=fecha, hora=hora, estado=estado)
+        db.session.add(turno)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('/ cargar_turno.html')
+
 
 @app.route('/vistap')
 def ver_pacientes():
     pacientes = Paciente.query.all()
-    print("pacientes ce mamo ")
-    return render_template('/vistap.html', pacientes=pacientes)    
+    return render_template('vistap.html', pacientes=pacientes)
 
-@app.route('/add', methods=['POST'])
-def add():
-    nombre_paciente = request.form['nombre_paciente']
-    nombre_medico = request.form['nombre_medico'] 
-    fecha_turno = request.form['fecha_turno']   
-    nuevo_paciente = Paciente(nombre_paciente=nombre_paciente, 
-                              nombre_medico=nombre_medico, 
-                              fecha_turno=fecha_turno)  # <-- corregido
-    db.session.add(nuevo_paciente)  
-    db.session.commit()
-    return redirect(url_for('index'))
 
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    paciente = Paciente.query.get_or_404(id)
+@app.route('/edit/<int:dni>', methods=['GET', 'POST'])
+def edit(dni):
+    paciente = Paciente.query.get_or_404(dni)
     if request.method == 'POST':
-        paciente.nombre_paciente = request.form['nombre_paciente']
-        paciente.nombre_medico = request.form['nombre_medico'] 
-        paciente.fecha_turno = request.form['fecha_turno']  # <-- corregido
+        paciente.nombre = request.form['nombre']
+        paciente.tel = request.form['tel']
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('edit.html', paciente=paciente)  # <-- corregido
+        return redirect(url_for('ver_pacientes'))
+    return render_template('edit.html', paciente=paciente)
 
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    paciente = Paciente.query.get_or_404(id)
-    db.session.delete(paciente)  # <-- corregido
+@app.route('/delete/<int:dni>')
+def delete(dni):
+    paciente = Paciente.query.get_or_404(dni)
+    db.session.delete(paciente)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('ver_pacientes'))
 
 if __name__ == '__main__':
     app.run(debug=True)
